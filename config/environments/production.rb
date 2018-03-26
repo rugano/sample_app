99b793e33145c91b322c119cbd6e3a157eb1982b
John Ita <jwaithaka746@gmail.com>
2018-03-26 08:42:16 +0000

    Added the domain name correctly in production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'guarded-badlands-56215.herokuapp.com',
+    :domain         => 'https://guarded-badlands-56215.herokuapp.com',
     :enable_starttls_auto => true
   }
   
