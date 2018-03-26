c996b18118dc7187b10bceb68b585240eab57596
John Ita <jwaithaka746@gmail.com>
2018-03-26 08:56:24 +0000

    Added the host name correctly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'https://guarded-badlands-56215.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -58,7 +58,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'https://guarded-badlands-56215.herokuapp.com',
+    :domain         => 'guarded-badlands-56215.herokuapp.com',
     :enable_starttls_auto => true
   }
   
