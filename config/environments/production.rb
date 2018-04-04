457056d6d54f699dfd1f926d98628c9a15cffcae
John Ita <jwaithaka746@gmail.com>
2018-04-04 13:25:51 +0000

    Corrected afew files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://guarded-badlands-56215.herokuapp.com/'
+  host = 'guarded-badlands-56215.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
